e8cd63d9db36290428c50661c11ae9763c3f4387
yongdo-kim <matthewkim@Matthewui-MacBookPro.local>
2023-02-21 17:34:13 +0900

    feat 노션에다가 타입 추가

--- 4_7_8.js
+++ 4_7_8.js
@@ -1,55 +1,56 @@
+console.log("------- 심볼형 START --------");
 /*
     심볼형
-    자바스크립트는 객체 프로퍼티 키로 오직 문자형과 심볼형만을 허용,숫자형, 불린형 모두 불가능
-    '심볼(symbol)'은 유일한 식별자(unique identifier)를 만들고 싶을 때 사용
-    심볼은 리터럴, for..in에서는 호출되지 않음
+      - 객체 프로퍼티 키로 오직 문자형과 심볼형만을 허용,숫자형, 불린형 모두 불가능
+      - 심볼(symbol)은 유일한 식별자(unique identifier)를 만들고 싶을 때 사용
+      - 심볼은 리터럴, for..in에서는 호출되지 않음
+      - 심볼 이름은 디버깅 시 아주 유용
 */
 
-let id = Symbol("id"); //심볼 이름은 디버깅 시 아주 유용
+let id = Symbol("id");
 let id1 = Symbol("id");
 let id2 = Symbol("id");
-console.log(id1 == id2); // false : Symbol()은 유일객체, 
+console.log(id1 == id2); // false : Symbol()은 유일객체,
 
 //숨김 프로퍼티
-let user = { // 서드파티 코드에서 가져온 객체
-    name: "John"
-  };
-  let id3 = Symbol("id");
-  user[id3] = 1; //심볼의 id를 key로 사용해서 접근 가능 
-  console.log( user[id3] ); 
-
+let user = {
+  // 서드파티 코드에서 가져온 객체
+  name: "John",
+};
+let id3 = Symbol("id");
+user[id3] = 1; //심볼의 id를 key로 사용해서 접근 가능
+console.log(user[id3]); //1
+console.log("------- 심볼형 END --------");
 
 //전역 심볼
 //Symbol.for("key");
 
-
+console.log("-------  객체를 원시형으로 변환하기 START --------");
 /*
   객체를 원시형으로 변환하기
-  객체 형 변환은 세 종류로 구분되는데, 'hint’라 불리는 값이 구분 기준 (목표로 하는 자료형)
-  hint는 "string", "number", "default" 중 하나가 될 수 있습니다.
-
+  - 객체 형 변환은 세 종류로 구분되는데, 'hint’라 불리는 값이 구분 기준 (목표로 하는 자료형)
+  - hint는 "string", "number", "default" 중 하나가 될 수 있습니다.
 */
 
 //Symbol.toPrimitive
 //toString과 valueOf는 심볼이 생기기 이전부터 존재해 왔던 ‘평범한’ 메서드
 let usertoPrimitive = {
-    name: "John",
-    money: 1000,
-    
-    toString() {
-        return `{name: "${this.name}"}`;
-      },
-    valueOf() {
-        return this.money;
-    },
-    [Symbol.toPrimitive](hint) {
-      console.log(`hint: ${hint}`);
-      return hint == "string" ? `{name: "${this.name}"}` : this.money;
-    }
+  name: "John",
+  money: 1000,
 
-    
-  };
+  toString() {
+    return `{name: "${this.name}"}`;
+  },
+  valueOf() {
+    return this.money;
+  },
+  [Symbol.toPrimitive](hint) {
+    console.log(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  },
+};
 
-  console.log(usertoPrimitive.toString()); //{name: "John"}
-  console.log(usertoPrimitive.valueOf()); //1000
-  
+console.log(usertoPrimitive.toString()); //{name: "John"}
+console.log(usertoPrimitive.valueOf()); //1000
+
+console.log("-------  객체를 원시형으로 변환하기 END --------");
--- 5_1_2_3_4.js
+++ 5_1_2_3_4.js
@@ -1,26 +1,34 @@
+console.log("-------  객체를 원시형으로 변환하기 START --------");
+
 /*
     원시값의 메서드 : ex) str.toUpperCase () 
     원시값 메서드가 만들어지는 방법
         - String,Number,Boolean, Symbol 래퍼 객체 사용
         - 원시값 -> 객체 생성 -> 메서드 호출 -> 객체 소멸 -> 원시값 반환 
-    
-    
 */
 
 //e
-let billion = 1e9;  // 10억, 1과 9개의 0
-console.log(billion);
-let ms = 1e-6; // 1에서 왼쪽으로 6번 소수점 이동
-console.log(ms);
+let billion = 1e9; 
+console.log(billion); //1000000000
+let ms = 1e-6; 
+console.log(ms); //0.000001
 
+console.log("-------  객체를 원시형으로 변환하기 END --------");
+
+
+console.log("-------  십진법. 이진법  START --------");
 //십진법. 이진법 
 //십진법 체계에서는 1/10, 2/10은 0.1, 0.2로 나눠질 수 있지만, 1/3이 무한소수로 됨.
-//이진법 체게에서는 0.1 또는 0.2를 정확하게 저장하는 방법은 없음 => 어림수 이용 => toFixed(n)
+//이진법 체계에서는 0.1 또는 0.2를 정확하게 저장하는 방법은 없음 => 어림수 이용 => toFixed(n)
 
 let sum = 0.1+0.2
 console.log(sum == 0.3); //0.30000000000000004 : false 
 console.log(sum.toFixed(3) == 0.3); //true 
 
+
+console.log("-------  십진법. 이진법  END --------");
+
+console.log("-------  isNaN. isFinite  START --------");
 //isNaN
 //NaN은 NaN 자기 자신을 포함하여 그 어떤 값과도 같지 않다는 점에서 독특, 
 console.log(NaN === NaN); //false
@@ -31,6 +39,7 @@ console.log(isNaN(NaN)) //true : 비교는 ===가 아닌 isNaN으로 한다.
 console.log(isFinite("15")); //true
 console.log(isFinite("str")); //false
 
+console.log("-------  isNaN. isFinite  END --------");
 
 /*
     문자열 : 문자열은 작은따옴표나 큰따옴표, 백틱으로 감쌀 수 있습니다. 
