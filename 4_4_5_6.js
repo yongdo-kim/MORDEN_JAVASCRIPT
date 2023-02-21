ae4d8f83453e61d762ce0e94b1958bbff53bb258
yongdo-kim <matthewkim@Matthewui-MacBookPro.local>
2023-02-21 16:55:59 +0900

    feat 노션에다가 타입 추가

--- 4_4_5_6.js
+++ 4_4_5_6.js
@@ -1,24 +1,26 @@
+console.log("------- 메서드와 This START --------");
+
 /*
     메서드와 this
-    this 값은 런타임에 결정됩니다. 컨텍스트에 따라 달라지죠. (객체가 누구냐에 따라 값 반환이 달라짐)
-    obj.f()를 호출했다면 this는 f를 호출하는 동안의 obj입니다.
-    자바스크립트에선 모든 함수에 this를 사용할 수 있습니다.
-    화살표 함수는 일반 함수와는 달리 ‘고유한’ this를 가지지 않습니다. 
-    화살표 함수에서 this를 참조하면, 화살표 함수가 아닌 ‘평범한’ 외부 함수에서 this 값을 가져옵니다.
+      - this는 런타임에 결정됩니다. this.value의 value는 this가 어떤 객체를 가리키고 있냐에 따라 달라짐.
+      - this는 함수 내에서 사용될 때, 해당 함수가 속한 객체를 참조하는 키워드임. 
+      - 자바스크립트에선 모든 함수에 this를 사용할 수 있습니다.
+      - 화살표 함수는 this가 없다. 화살표 함수 안에서 this를 사용하면 외부 this를 사용한다. (외부 : 화살표 함수를 감싸는 함수) 
+      - 일반 함수는 this가 있다. 프로퍼티가 없는 상태로 this를 호출하면 undifined가 나온다. 
 */
 
 let user = {
-// "sayHi: function(){console.log()}"과 동일합니다.
-    name:"yongdo",
-    sayHi() { 
-      console.log(`${this.name}`); // 객체 프로퍼티에 할당된 함수를 메서드(method) 라고 부릅니다.
-    }
-  };
-user.sayHi(); //this는 user로 동적변경 
+  name: "yongdo",
+  // "sayHi: function(){console.log()}"과 동일합니다.
+  sayHi() {
+    console.log(`${this.name}`); //이 this는 user를 의미함.
+  },
+};
+user.sayHi(); //yongdo
 
-//아무것도 없다면 this는 undefined 반환 
+//아무것도 없다면 this는 undefined 반환
 function showThisByNothing() {
-    console.log(this.name); 
+  console.log(this.name); //undefined
 }
 showThisByNothing();
 
@@ -26,21 +28,22 @@ showThisByNothing();
 let freeThis = {
   name: "보라",
   sayHiArrow() {
-    let arrow = () => console.log(this.name); //화살표 함수는 this가 없음 외부 this를 사용
+    let arrow = () => console.log(this.name); //화살표 함수는 this가 없음 화살표 함수 위의 함수를 this로 판단.
     arrow();
   },
   sayHiLet() {
     function hihi() {
-        console.log(this.name); //hihi()내부 함수에 있는 this를 호출 
+      console.log(this.name); //hihi()내부 함수에 있는 this를 호출
     }
     hihi();
-  }
+  },
 };
 
 freeThis.sayHiArrow(); // 보라
 freeThis.sayHiLet(); // undefined
+console.log("------- 메서드와 This END --------");
 
-
+console.log("------- new 연산자와 생성자 함수 START --------");
 /*
     new 연산자와 생성자 함수
     함수 이름의 첫 글자는 대문자로 시작합니다.
@@ -50,24 +53,26 @@ freeThis.sayHiLet(); // undefined
 
 */
 
-
 function ConstructorUser(name) {
-    // this = {};  (빈 객체가 암시적으로 만들어짐) -(1) 작동원리
+  // this = {};  (빈 객체가 암시적으로 만들어짐) -(1) 작동원리
 
-    // 새로운 프로퍼티를 this에 추가함 - (2) 작동원리
-    this.name = name;
-    this.isAdmin = false;
-  
-    // return this;  (this가 암시적으로 반환됨) - (3) 작동원리
-  }
+  // 새로운 프로퍼티를 this에 추가함 - (2) 작동원리
+  this.name = name;
+  this.isAdmin = false;
+
+  // return this;  (this가 암시적으로 반환됨) - (3) 작동원리
+}
 
 let constructorUserser = new ConstructorUser("보라");
 console.log(constructorUserser.name); //보라
 
+console.log("------- new 연산자와 생성자 함수 END --------");
 
+console.log("-------  옵셔널 체이닝  START --------");
 /* 
     옵셔널 체이닝 
     ?.은 ?.'앞’의 평가 대상이 undefined나 null이면 평가를 멈추고 undefined를 반환합니다.
 */
 let optionalUser = {}; // 주소 정보가 없는 사용자
 console.log(optionalUser?.address?.street);
+console.log("-------  옵셔널 체이닝  END --------");
